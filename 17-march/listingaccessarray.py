5415825248e15cc519a0cbd8ebb7f0ac45797804
syedanasali09 <syedanasali75@gmail.com>
2023-03-16 23:48:02 -0700

    PRACTICE

---
+++ 17-march/listingaccessarray.py
@@ -0,0 +1,13 @@
+list1 = ["joseph", "gleen", "sally"]
+print(list1[0])  
+print(list1[1])  
+print(list1[2])
+list2=[[1,2],4,5]
+print(list2[0])
+print(list2[1])
+print(list2[2])
+print(list2[0][0])
+print(list2[0][1])
+index = int(input("Enter the index of the element u want to access (0 to 2): "))
+print(list1[index])
+print(list2[index])
