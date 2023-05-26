f6b8a4b01482ac6b1aad40871dfdc52d0c5bf7b0
syedanasali09 <syedanasali75@gmail.com>
2023-05-26 17:44:30 +0500

    lab_OhtPrep

---
+++ Lab-OHT/class.py
@@ -0,0 +1,16 @@
+class Person:         # class created
+      def __init__ (self, name, age, country, city):       #constructor function built in (self)parameter
+          self.name = name
+          self.age = age
+          self.country = country
+          self.city = city
+
+p = Person('Anas','21','pakistan', 'karachi')   #object
+print(p.name)   
+print(p.age)
+print(p.country)
+print(p.city)
+
+
+
+      
